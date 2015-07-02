4d8167288244dbb73f58b9cee930df26bd92dceb
akumar <akumar@central1.com>
2015-07-02 16:13:50 -0700

    first commit

---
+++ Delegates Challange/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Delegates Challange
+//
+//  Created by Amit Kumar on 2015-07-02.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var zipCodeText: UITextField!
+    @IBOutlet weak var cashValueText: UITextField!
+    @IBOutlet weak var switchCodeText: UITextField!
+    @IBOutlet weak var switchButton: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+}
+
---
+++ Delegates Challange/ZipCodeDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ZipCodeDelegate.swift
+//  Delegates Challange
+//
+//  Created by Amit Kumar on 2015-07-02.
+//  Copyright (c) 2015 Agranee Solutions Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ZipCodeDelegate : NSObject, UITextFieldDelegate {
+    
+}
