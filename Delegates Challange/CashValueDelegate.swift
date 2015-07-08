c571da3cd8724ec84ea28acb8eaf84ebf99fd592
akumar <akumar@central1.com>
2015-07-08 15:58:33 -0700

    added Cash Value

--- Delegates Challange/CashValueDelegate.swift
+++ Delegates Challange/CashValueDelegate.swift
@@ -11,40 +11,28 @@ import UIKit
 class CashValueDelegate : NSObject, UITextFieldDelegate {
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
-        var result = true
-        let prospectiveText = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
-        var newText: NSString = textField.text
+        // Construct the text that will be in the field if this change is accepted
+        var newText = textField.text as NSString
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
         
-        if ( newText.length > 0) {
-            let disallowedCharacterSet = NSCharacterSet(charactersInString: "0123456789.").invertedSet
-            let replacementStringIsLegal = string.rangeOfCharacterFromSet(disallowedCharacterSet) == nil
-            
-            let scanner = NSScanner(string: prospectiveText)
-            let resultingTextIsNumeric = scanner.scanDecimal(nil) && scanner.atEnd
-            result = replacementStringIsLegal && resultingTextIsNumeric
-            textField.text = formatAsPrice(prospectiveText) as String
-        }
+        var num: NSNumber = NSNumber(integer: newText.integerValue)
+        textField.text = convertCurrency(num)
+
         
-        return result
+        return true
         
+    }
+    
+    func convertCurrency(num : NSNumber) -> String {
+            var formatter = NSNumberFormatter()
+            formatter.maximumFractionDigits = 1
+            formatter.numberStyle = .CurrencyStyle
+            formatter.locale = NSLocale.currentLocale()
+            return formatter.stringFromNumber(num)!
     }
     
     func textFieldDidBeginEditing(textField: UITextField) {
         textField.text = ""
     }
     
-    func formatAsPrice(priceString: NSString) -> NSString {
-        
-        var price: NSNumber = NSNumber(double: priceString.doubleValue) ;
-        
-        var formatter = NSNumberFormatter()
-        formatter.numberStyle = .CurrencyStyle
-        formatter.stringFromNumber(price)
-        
-        formatter.locale = NSLocale.autoupdatingCurrentLocale()
-        var priceNumber : NSString = formatter.stringFromNumber(price)!
-        
-        return priceNumber
-        
-    }
 }
